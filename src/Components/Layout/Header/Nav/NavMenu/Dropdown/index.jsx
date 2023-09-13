a24485511ee8082328099c7b3d0ec78a3c70e20a
MariceAshtiani <marice95@hotmail.no>
2023-09-13 19:16:31 +0200

    tried to create a drop-down menu

---
+++ src/Components/Layout/Header/Nav/NavMenu/Dropdown/index.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import StyledDropdown from "./styled";
+import { NavLink } from "react-router-dom";
+
+export default function DropdownMenu({ items }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <StyledDropdown>
+        <div className="dropdown"
+        onClick={() => toggleDropdown()}>
+            <span className="dropdown-button">Profile</span>
+            {isOpen && (
+                <ul className="dropdown-menu">
+                    {items.map((item) => (
+                        <li key={item.id}>
+                            <NavLink to={item.link}>{item.label}</NavLink>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+        </StyledDropdown>
+    );
+}
