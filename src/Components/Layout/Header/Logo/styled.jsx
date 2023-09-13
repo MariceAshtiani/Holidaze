ccfcd0597b9f517c4882601d0865852e5e962675
MariceAshtiani <marice95@hotmail.no>
2023-09-13 19:15:56 +0200

    added some styles to the nav

--- src/Components/Layout/Header/Logo/styled.jsx
+++ src/Components/Layout/Header/Logo/styled.jsx
@@ -2,7 +2,17 @@ import styled from "styled-components";
 
 export default styled.div`
         font-family: "Segoe UI";
-        font-sixe: 3rem;
+        font-size: 3rem;
+        padding-right: 20px;
+        font-weight: 900;
+
+        a {
+            color: darkorange;
+        }
+
+        a:hover {
+            color: white;
+        }
 
         @media (max-width: ${({ theme }) => theme.mobile}) {
             font-size: 1.5rem;
---
+++ src/Components/Layout/Header/Nav/NavMenu/styled.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export default styled.div`
+    cursor: pointer;
+
+    .nav-links {
+        display: flex;
+        list-style: none;
+        padding: 0;
+    }
+
+
+    .nav-links li {
+        margin-left: 20px;
+    }
+
+    a {
+            font-weight: bold;
+            color: darkorange;
+        }
+
+    .nav-links li:hover a{
+        color: white;
+    }
+
+    .active {
+        text-decoration: underline;
+        color: black;
+    }
+
+    @media (max-width: ${({ theme }) => theme.tablet}) {
+        ul {
+        }
+    }
+
+    @media (max-width: ${({ theme }) => theme.mobile}) {
+        ul {
+            font-size: 1rem;
+        }
+    }
+`;
