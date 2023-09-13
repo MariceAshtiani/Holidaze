e75049d4e78ab79529e34c4e4597055067ef24cc
MariceAshtiani <marice95@hotmail.no>
2023-09-13 19:16:58 +0200

    added the layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import Layout from "./Components/Layout"
 import Home from "./Pages/Home";
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
+import Profile from "./Pages/Profile";
 
 
 export default function App() {
 
   return (
     <Routes>
-      <Route path="/">
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
+        <Route path="profile" element={<Profile />} />
       </Route>
     </Routes>
   )
