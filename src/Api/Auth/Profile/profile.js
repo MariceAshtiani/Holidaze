f0baa0763c1885ed6977b0ac4f58a319329ebef6
MariceAshtiani <marice95@hotmail.no>
2023-09-26 21:28:37 +0200

    added some changes to api-auth

--- src/Api/Auth/Profile/profile.js
+++ src/Api/Auth/Profile/profile.js
@@ -2,19 +2,12 @@ import { baseUrl } from "../../constants";
 import { headers } from "../headers";
 import { profiles } from "../../constants";
 
-export async function getProfile(credentials) {
+export async function getProfile(name) {
     const Url = baseUrl;
-
-    const storedData = localStorage.getItem("UserData");
-    const data = JSON.parse(storedData);
-
-    const name = data.name;
-
-    const endPoint = profiles + `/${name}?_bookings=true&_venues=true`;
+    const endPoint = profiles + `/${name}/venues?_bookings=true`;
 
     return fetch(Url + endPoint, {
         method: "GET",
         headers: headers("application/json"),
-        body: JSON.stringify(credentials),
     }).then((data) => data.json());
 };
