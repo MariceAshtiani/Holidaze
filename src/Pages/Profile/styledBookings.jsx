223f388a1266f7ed9527dcf34cb04bab603a4ea7
MariceAshtiani <marice95@hotmail.no>
2023-10-03 15:24:36 +0200

    styled venues/bookings profile

---
+++ src/Pages/Profile/styledBookings.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export default styled.div`
+
+@media (max-width: ${({ theme }) => theme.tablet}) {
+
+  
+}
+
+@media (max-width: ${({ theme }) => theme.mobile}) {
+ h2 {
+    text-align: center;
+ }
+
+}`
