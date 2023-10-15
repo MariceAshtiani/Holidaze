daacd316e8091d0b225b788f6ca664e233b0d039
MariceAshtiani <marice95@hotmail.no>
2023-10-15 16:35:21 +0200

    removed font-size

--- src/Pages/Listings/styled.jsx
+++ src/Pages/Listings/styled.jsx
@@ -4,14 +4,5 @@ export default styled.h2`
     
     text-align: center;
     margin: 30px;
-    font-size: 3rem;
     margin-bottom: 10%;
-
-    @media (max-width: ${({ theme }) => theme.tablet }) {
-        font-size: 2rem;
-    }
-
-    @media (max-width: ${({ theme }) => theme.mobile }) {
-        font-size: 1.5rem;
-    }
 `
